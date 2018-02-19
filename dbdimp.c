5750fe7536eaace1b36670c32d6c79fff05e583b
Pali <pali@cpan.org>
2018-02-19 12:33:34 +0100

    Distinguish between NULL and "" in mysql_real_connect()
    Some arguments (host, user, passwd, db, port or unix_socket) passed to
    mysql_real_connect() have different meaning when are NULL and when are "".
    But DBD::MariaDB currently converts everything to nul term strings which
    disallow usage of NULL arguments.
    
    This commit fixes it by mapping Perl's undef to C's NULL and Perl's empty
    string to C's "".
    
    See: https://dev.mysql.com/doc/refman/en/mysql-real-connect.html
    See: https://mariadb.com/kb/en/library/mysql_real_connect/
    
    Fixes: https://github.com/perl5-dbi/DBD-mysql/issues/167
    Fixes: https://rt.cpan.org/Public/Bug/Display.html?id=108650

--- dbdimp.c
+++ dbdimp.c
@@ -2519,17 +2519,15 @@ MYSQL *mariadb_dr_connect(
 */
 static char *safe_hv_fetch(pTHX_ HV *hv, const char *name, int name_length)
 {
-  SV** svp;
-  STRLEN len;
-  char *res= NULL;
+  SV** svp = hv_fetch(hv, name, name_length, FALSE);
+  if (!svp || !*svp)
+    return NULL;
 
-  if ((svp= hv_fetch(hv, name, name_length, FALSE)))
-  {
-    res= SvPV(*svp, len);
-    if (!len)
-      res= NULL;
-  }
-  return res;
+  SvGETMAGIC(*svp);
+  if (!SvOK(*svp))
+    return NULL;
+
+  return SvPV_nolen(*svp);
 }
 
 /*
