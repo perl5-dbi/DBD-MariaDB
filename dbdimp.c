perl5-dbi/DBD-MariaDB

commit 61dfddbf48b9c52fe06f5c3a6ba3840dc34c202e

Fix last insert id workaround for MySQL 5.7 and 8.0 client versions
MySQL 5.7 below 5.7.18 and MySQL 8.0.0 are affected by Bug #78778.
mysql_insert_id() is reset to 0 after performing SELECT operation.

As a workaround prior to issuing mysql query we store value of last insert
id. If query returns result set then we know that it was SELECT operation
and so after that we restore previous value of last insert id.

MySQL 5.7, MySQL 8.0, MySQL Connector/C 6.1.5 and higher are affected by
Bug #89139. mysql_insert_id() is reset to 0 after calling mysql_ping() C
function.

As a workaround prior to calling mysql_ping() function we store value of
last insert id. After function finish we restore previous value of last
insert id.

DBD::MariaDB had already second workaround but with wrong version check.
This commit therefore fix version check for second workaround and declare
helper macros for determining if workaround is needed.

Applying both workaround fixes $dbh->last_insert_id() method and now can be
called also when compiled with those affected MySQL client versions, even
after $dbh->ping() method or performing SELECT operation. So skip blocks
for last insert id tests are now removed and tests are enabled for all
MySQL client versions.

Fixes: 26ff601f518c76b8900764780e867b74e20824a1
See: https://bugs.mysql.com/bug.php?id=78778
See: https://bugs.mysql.com/bug.php?id=89139

dbdimp.c
@@ -3919,6 +3919,17 @@ my_ulonglong mariadb_st_internal_execute(
         rows = 0;
     }
   } else {
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+      /*
+       * mysql_insert_id() returns incorrect value after SELECT operation.
+       * As a workaround prior to issuing mysql query we store value of
+       * last insert id. If query returns result set then we know that it
+       * was SELECT operation and so after that we restore previous value
+       * of last insert id.
+       */
+      my_ulonglong insertid = mysql_insert_id(*svsock);
+#endif
+
       if ((mysql_real_query(*svsock, sbuf, slen))  &&
           (!mariadb_db_reconnect(h, NULL) ||
            (mysql_real_query(*svsock, sbuf, slen))))
@@ -3936,6 +3947,11 @@ my_ulonglong mariadb_st_internal_execute(
           else {
             rows = mysql_affected_rows(*svsock);
           }
+
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+          if (*result)
+            (*svsock)->insert_id = insertid;
+#endif
       }
   }
 

MariaDB.xs
@@ -372,21 +372,20 @@ ping(dbh)
     SV* dbh;
   CODE:
     {
-/* MySQL 5.7 below 5.7.18 is affected by Bug #78778.
- * MySQL 5.7.18 and higher (including 8.0.3) is affected by Bug #89139.
- *
- * Once Bug #89139 is fixed we can adjust the upper bound of this check.
- *
- * https://bugs.mysql.com/bug.php?id=78778
- * https://bugs.mysql.com/bug.php?id=89139 */
-#if !defined(MARIADB_BASE_VERSION) && MYSQL_VERSION_ID >= 50718
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_PING
+      /*
+       * mysql_insert_id() returns incorrect value after mysql_ping() C function.
+       * As a workaround prior to calling mysql_ping() function we store value
+       * of last insert id. After function finish we restore previous value of
+       * last insert id.
+       */
       my_ulonglong insertid;
 #endif
       D_imp_dbh(dbh);
       ASYNC_CHECK_XS(dbh);
       if (!imp_dbh->pmysql)
         XSRETURN_NO;
-#if !defined(MARIADB_BASE_VERSION) && MYSQL_VERSION_ID >= 50718
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_PING
       insertid = mysql_insert_id(imp_dbh->pmysql);
 #endif
       RETVAL = (mysql_ping(imp_dbh->pmysql) == 0);
@@ -395,7 +394,7 @@ ping(dbh)
         if (mariadb_db_reconnect(dbh, NULL))
           RETVAL = (mysql_ping(imp_dbh->pmysql) == 0);
       }
-#if !defined(MARIADB_BASE_VERSION) && MYSQL_VERSION_ID >= 50718
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_PING
       imp_dbh->pmysql->insert_id = insertid;
 #endif
     }

dbdimp.h
@@ -313,6 +313,25 @@ PERL_STATIC_INLINE UV SvUV_nomg(pTHX_ SV *sv)
 #endif
 
 /*
+ * MySQL 5.7 below 5.7.18 and MySQL 8.0.0 are affected by Bug #78778.
+ * mysql_insert_id() is reset to 0 after performing SELECT operation.
+ * https://bugs.mysql.com/bug.php?id=78778
+ */
+#if !defined(MARIADB_BASE_VERSION) && ((MYSQL_VERSION_ID >= 50700 && MYSQL_VERSION_ID <= 50717) || MYSQL_VERSION_ID == 80000)
+#define HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+#endif
+
+/*
+ * MySQL 5.7, MySQL 8.0, MySQL Connector/C 6.1.5 and higher are affected by Bug #89139.
+ * mysql_insert_id() is reset to 0 after calling mysql_ping() C function.
+ * Once Bug #89139 is fixed we can adjust the upper bound of this check.
+ * https://bugs.mysql.com/bug.php?id=89139
+ */
+#if !defined(MARIADB_BASE_VERSION) && MYSQL_VERSION_ID >= 50700 && (MYSQL_VERSION_ID < 60100 || MYSQL_VERSION_ID >= 60105)
+#define HAVE_BROKEN_INSERT_ID_AFTER_PING
+#endif
+
+/*
  * Check which SSL settings are supported by API at compile time
  */
 

t/31insertid.t
@@ -43,11 +43,7 @@ is $dbh->last_insert_id(undef, undef, undef, undef), 1, "insert id == last_inser
 ok $sth->execute("Patrick");
 
 $dbh->ping();
-SKIP: {
-  skip 'using libmysqlclient 5.7 or up we now have an empty dbh insertid',
-    1, if ($dbh->{mariadb_clientversion} >= 50700 && $dbh->{mariadb_clientversion} < 50718) || ($dbh->{mariadb_clientversion} >= 60105 && $dbh->{mariadb_clientversion} < 69999) || $dbh->{mariadb_clientversion} == 80000;
   is $dbh->last_insert_id(undef, undef, undef, undef), 2, "insert id == last_insert_id()";
-}
 
 ok (my $sth2= $dbh->prepare("SELECT max(id) FROM dbd_mysql_t31"));
 
@@ -60,12 +56,8 @@ ok ($max_id= $sth2->fetch());
 
 ok defined $max_id;
 
-SKIP: {
-  skip 'using libmysqlclient 5.7 below 5.7.18 we now have an empty dbh insertid',
-    1, if ($dbh->{mariadb_clientversion} >= 50700 && $dbh->{mariadb_clientversion} < 50718) || ($dbh->{mariadb_clientversion} >= 60105 && $dbh->{mariadb_clientversion} < 69999) || $dbh->{mariadb_clientversion} == 80000;
   cmp_ok $dbh->{mariadb_insertid}, '==', $max_id->[0],
     "dbh insert id $dbh->{'mariadb_insertid'} == max(id) $max_id->[0] in dbd_mysql_t31";
-}
 cmp_ok $sth->{mariadb_insertid}, '==', $max_id->[0],
   "sth insert id $sth->{'mariadb_insertid'} == max(id) $max_id->[0]  in dbd_mysql_t31";
 

commit 89dd04a6c1b602b0a49f82f4e37b1425b3527646

Fix last insert id workaround also for prepared statements
MySQL 5.7 below 5.7.18 and MySQL 8.0.0 are affected by Bug #78778.
mysql_insert_id() is reset to 0 after performing SELECT operation.

This applies also for execution of prepared SELECT statement.

Fixes: 61dfddbf48b9c52fe06f5c3a6ba3840dc34c202e
See: https://bugs.mysql.com/bug.php?id=78778

dbdimp.c
@@ -4288,12 +4288,17 @@ my_ulonglong mariadb_st_internal_execute41(
                                         )
 {
   dTHX;
+  int store_retval;
   int execute_retval;
   unsigned int i, num_fields;
   MYSQL_STMT *stmt = *stmt_ptr;
   my_ulonglong rows=0;
   bool reconnected = FALSE;
   D_imp_xxh(h);
+
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+  my_ulonglong insertid;
+#endif
 
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh),
@@ -4337,6 +4342,18 @@ my_ulonglong mariadb_st_internal_execute41(
 
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t\tmariadb_st_internal_execute41 calling mysql_execute\n");
+
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+  /*
+   * mysql_insert_id() returns incorrect value after SELECT operation.
+   * As a workaround prior to issuing mysql query we store value of
+   * last insert id. If query returns result set then we know that it
+   * was SELECT operation and so after that we restore previous value
+   * of last insert id. Restoring needs to be done after call to the
+   * mysql_stmt_store_result() function as it may clear insert id.
+   */
+  insertid = mysql_insert_id(*svsock);
+#endif
 
   if (!reconnected)
   {
@@ -4407,11 +4424,14 @@ my_ulonglong mariadb_st_internal_execute41(
         break;
       }
     }
-    /* Get the total rows affected and return */
-    if (mysql_stmt_store_result(stmt))
+    store_retval = mysql_stmt_store_result(stmt);
+#ifdef HAVE_BROKEN_INSERT_ID_AFTER_SELECT
+    (*svsock)->insert_id = insertid;
+#endif
+    if (store_retval)
       goto error;
-    else
-      rows= mysql_stmt_num_rows(stmt);
+    /* Get the total rows affected and return */
+    rows = mysql_stmt_num_rows(stmt);
   }
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh),