perl5-dbi/DBD-MariaDB

commit 5034cd71c2179fc166bc55fd5633d8a3f02fced2

Remove poll() based implementation of mariadb_dr_socket_ready()
One select() based implementation which works on both POSIX and WIN32
systems for just one file descriptor is enough.

socket.c
@@ -6,13 +6,9 @@
 #include <windows.h>
 #include <winsock.h>
 #endif
-
 #include <mysql.h>
-
-#ifndef _WIN32
-#include <poll.h>
+#include <stddef.h>
 #include <errno.h>
-#endif
 
 /*
  * Warning: Native socket code must be outside of dbdimp.c and dbdimp.h because
@@ -22,12 +18,9 @@
 
 int mariadb_dr_socket_ready(my_socket fd)
 {
-  int retval;
-
-#ifdef _WIN32
-  /* Windows does not have poll(), so use select() instead */
   struct timeval timeout;
   fd_set fds;
+  int retval;
 
   FD_ZERO(&fds);
   FD_SET(fd, &fds);
@@ -36,15 +29,6 @@ int mariadb_dr_socket_ready(my_socket fd)
   timeout.tv_usec = 0;
 
   retval = select(fd+1, &fds, NULL, NULL, &timeout);
-#else
-  struct pollfd fds;
-
-  fds.fd = fd;
-  fds.events = POLLIN;
-
-  retval = poll(&fds, 1, 0);
-#endif
-
   if (retval < 0) {
 #ifdef _WIN32
     /* Windows does not update errno */

commit 5a20d0bb5fdb7824bbe56e6521b071a96b564d86

Fix compilation on OpenBSD and HP-UX
For using select() function on POSIX systems it is needed to include more
header files.

Fixes: 5034cd71c2179fc166bc55fd5633d8a3f02fced2

socket.c
@@ -10,6 +10,12 @@
 #include <stddef.h>
 #include <errno.h>
 
+#ifndef _WIN32
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
 /*
  * Warning: Native socket code must be outside of dbdimp.c and dbdimp.h because
  *          perl header files redefine socket function. This file must not