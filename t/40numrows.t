perl5-dbi/DBD-MariaDB

commit aed157b4c56ee12cd40b23d2c9a3f514cca4d455

Fix and simplify code which uses number of returned or affected rows
All mysql API functions returns rows as my_ulonglong type, so use this type
in all places in code. For conversion of my_ulonglong value to SV* variable
always use my_ulonglong2sv() function. To indicate error always use value
(my_ulonglong)-1.

MariaDB.xs
@@ -80,7 +80,7 @@ type_info_all(dbh)
 }
 
 
-void
+SV *
 do(dbh, statement, attr=Nullsv, ...)
   SV *        dbh
   SV *	statement
@@ -91,7 +91,7 @@ do(dbh, statement, attr=Nullsv, ...)
   D_imp_dbh(dbh);
   int num_params= (items > 3 ? items - 3 : 0);
   int i;
-  int retval;
+  my_ulonglong retval;
   STRLEN slen;
   char *str_ptr;
   struct imp_sth_ph_st* params= NULL;
@@ -197,7 +197,7 @@ do(dbh, statement, attr=Nullsv, ...)
       {
         mariadb_dr_do_error(dbh, mysql_stmt_errno(stmt), mysql_stmt_error(stmt)
                  ,mysql_stmt_sqlstate(stmt));
-        retval=-2;
+        retval = (my_ulonglong)-1;
       }
       mysql_stmt_close(stmt);
       stmt= NULL;
@@ -246,7 +246,7 @@ do(dbh, statement, attr=Nullsv, ...)
       mysql_stmt_close(stmt);
       stmt= NULL;
 
-      if (retval == -2) /* -2 means error */
+      if (retval == (my_ulonglong)-1) /* -1 means error */
       {
         SV *err = DBIc_ERR(imp_dbh);
         if (!disable_fallback_for_server_prepare && SvIV(err) == ER_UNSUPPORTED_PS)
@@ -289,7 +289,7 @@ do(dbh, statement, attr=Nullsv, ...)
     result= 0;
   }
 #if MYSQL_VERSION_ID >= MULTIPLE_RESULT_SET_VERSION
-  if (retval != -2 && !async) /* -2 means error */
+  if (retval != (my_ulonglong)-1 && !async) /* -1 means error */
     {
       /* more results? -1 = no, >0 = error, 0 = yes (keep looping) */
       while ((next_result_rc= mysql_next_result(imp_dbh->pmysql)) == 0)
@@ -309,18 +309,20 @@ do(dbh, statement, attr=Nullsv, ...)
               mariadb_dr_do_error(dbh, mysql_errno(imp_dbh->pmysql),
                        mysql_error(imp_dbh->pmysql),
                        mysql_sqlstate(imp_dbh->pmysql));
-              retval= -2;
+              retval = (my_ulonglong)-1;
           }
     }
 #endif
-  /* remember that dbd_st_execute must return <= -2 for error	*/
-  if (retval == 0)		/* ok with no rows affected	*/
-    XST_mPV(0, "0E0");	/* (true but zero)		*/
-  else if (retval < -1)	/* -1 == unknown number of rows	*/
-    XST_mUNDEF(0);		/* <= -2 means error   		*/
-  else
-    XST_mIV(0, retval);	/* typically 1, rowcount or -1	*/
+
+  if (retval == 0)                      /* ok with no rows affected     */
+    XSRETURN_PV("0E0");                 /* (true but zero)              */
+  else if (retval == (my_ulonglong)-1)  /* -1 means error               */
+    XSRETURN_UNDEF;
+
+  RETVAL = my_ulonglong2sv(retval);
 }
+  OUTPUT:
+    RETVAL
 
 
 SV*
@@ -392,22 +394,24 @@ void mariadb_sockfd(dbh)
         }
     }
 
-void mariadb_async_result(dbh)
+SV *
+mariadb_async_result(dbh)
     SV* dbh
-  PPCODE:
+  CODE:
     {
-        int retval;
+        my_ulonglong retval;
 
         retval = mariadb_db_async_result(dbh, NULL);
 
-        if(retval > 0) {
-            XSRETURN_IV(retval);
-        } else if(retval == 0) {
+        if (retval == 0)
             XSRETURN_PV("0E0");
-        } else {
+        else if (retval == (my_ulonglong)-1)
             XSRETURN_UNDEF;
-        }
+
+        RETVAL = my_ulonglong2sv(retval);
     }
+  OUTPUT:
+    RETVAL
 
 void mariadb_async_ready(dbh)
     SV* dbh
@@ -459,50 +463,40 @@ more_results(sth)
     OUTPUT:
       RETVAL
 
-void
+SV *
 rows(sth)
     SV* sth
   CODE:
     D_imp_sth(sth);
-    char buf[64];
     D_imp_dbh_from_sth;
     if(imp_dbh->async_query_in_flight) {
-        if(mariadb_db_async_result(sth, &imp_sth->result) < 0) {
+        if (mariadb_db_async_result(sth, &imp_sth->result) == (my_ulonglong)-1) {
             XSRETURN_UNDEF;
         }
     }
+    RETVAL = my_ulonglong2sv(imp_sth->row_num);
+  OUTPUT:
+    RETVAL
 
-  /* fix to make rows able to handle errors and handle max value from 
-     affected rows.
-     if mysql_affected_row returns an error, it's value is 18446744073709551614,
-     while a (my_ulonglong)-1 is  18446744073709551615, so we have to add 1 to
-     imp_sth->row_num to know if there's an error
-  */
-  if (imp_sth->row_num+1 ==  (my_ulonglong) -1)
-    sprintf(buf, "%d", -1);
-  else
-    sprintf(buf, "%llu", imp_sth->row_num);
-
-  ST(0) = sv_2mortal(newSVpvn(buf, strlen(buf)));
-
-int mariadb_async_result(sth)
+SV *
+mariadb_async_result(sth)
     SV* sth
   CODE:
     {
         D_imp_sth(sth);
-        int retval;
+        my_ulonglong retval;
 
         retval= mariadb_db_async_result(sth, &imp_sth->result);
 
-        if(retval > 0) {
-            imp_sth->row_num = retval;
-            XSRETURN_IV(retval);
-        } else if(retval == 0) {
-            imp_sth->row_num = retval;
-            XSRETURN_PV("0E0");
-        } else {
+        if (retval == (my_ulonglong)-1)
             XSRETURN_UNDEF;
-        }
+
+        imp_sth->row_num = retval;
+
+        if (retval == 0)
+            XSRETURN_PV("0E0");
+
+        RETVAL = my_ulonglong2sv(retval);
     }
   OUTPUT:
     RETVAL

dbdimp.c
@@ -4196,12 +4196,12 @@ my_ulonglong mariadb_st_internal_execute(
     if (!slen)
     {
       mariadb_dr_do_error(h, JW_ERR_QUERY, "Missing table name" ,NULL);
-      return -2;
+      return -1;
     }
     if (!(table= malloc(slen+1)))
     {
       mariadb_dr_do_error(h, JW_ERR_MEM, "Out of memory" ,NULL);
-      return -2;
+      return -1;
     }
 
     strncpy(table, sbuf, slen);
@@ -4222,7 +4222,7 @@ my_ulonglong mariadb_st_internal_execute(
     {
       mariadb_dr_do_error(h, mysql_errno(svsock), mysql_error(svsock)
                ,mysql_sqlstate(svsock));
-      return -2;
+      return -1;
     }
 
     return 0;
@@ -4233,7 +4233,7 @@ my_ulonglong mariadb_st_internal_execute(
        (!mariadb_db_reconnect(h) ||
         (mysql_send_query(svsock, sbuf, slen))))
     {
-        rows = -2;
+        rows = -1;
     } else {
         rows = 0;
     }
@@ -4242,21 +4242,18 @@ my_ulonglong mariadb_st_internal_execute(
           (!mariadb_db_reconnect(h)  ||
            (mysql_real_query(svsock, sbuf, slen))))
       {
-        rows = -2;
+        rows = -1;
       } else {
           /** Store the result from the Query */
           *result= use_mysql_use_result ?
             mysql_use_result(svsock) : mysql_store_result(svsock);
 
           if (mysql_errno(svsock))
-            rows = -2;
+            rows = -1;
           else if (*result)
             rows = mysql_num_rows(*result);
           else {
             rows = mysql_affected_rows(svsock);
-            /* mysql_affected_rows(): -1 indicates that the query returned an error */
-            if (rows == (my_ulonglong)-1)
-              rows = -2;
           }
       }
   }
@@ -4264,7 +4261,8 @@ my_ulonglong mariadb_st_internal_execute(
   if (salloc)
     Safefree(salloc);
 
-  if(rows == (my_ulonglong)-2) {
+  if (rows == (my_ulonglong)-1)
+  {
     mariadb_dr_do_error(h, mysql_errno(svsock), mysql_error(svsock), 
              mysql_sqlstate(svsock));
     if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
@@ -4407,7 +4405,7 @@ error:
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh),
                   "\t<- mariadb_st_internal_execute41\n");
-  return -2;
+  return -1;
 
 }
 #endif
@@ -4431,7 +4429,6 @@ error:
 IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
 {
   dTHX;
-  char actual_row_num[64];
   int i;
   D_imp_dbh_from_sth;
   D_imp_xxh(sth);
@@ -4486,7 +4483,7 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
         mariadb_dr_do_error(sth, ER_UNSUPPORTED_PS,
                  "\"mariadb_use_result\" not supported with server side prepare",
                  "HY000");
-        return 0;
+        return -2;
       }
       use_server_side_prepare = 0;
     }
@@ -4501,7 +4498,7 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
                                                     imp_sth->bind,
                                                     &imp_sth->has_been_bound
                                                    );
-      if (imp_sth->row_num == (my_ulonglong)-2) /* -2 means error */
+      if (imp_sth->row_num == (my_ulonglong)-1) /* -1 means error */
       {
         SV *err = DBIc_ERR(imp_xxh);
         if (!disable_fallback_for_server_prepare && SvIV(err) == ER_UNSUPPORTED_PS)
@@ -4532,7 +4529,7 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
     }
   }
 
-  if (imp_sth->row_num+1 != (my_ulonglong)-1)
+  if (imp_sth->row_num != (my_ulonglong)-1)
   {
     if (!imp_sth->result)
     {
@@ -4559,17 +4556,17 @@ IV mariadb_st_execute_iv(SV* sth, imp_sth_t* imp_sth)
 
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
   {
-    /* 
-      PerlIO_printf doesn't always handle imp_sth->row_num %llu 
-      consistently!!
-    */
-    sprintf(actual_row_num, "%llu", imp_sth->row_num);
     PerlIO_printf(DBIc_LOGPIO(imp_xxh),
-                  " <- mariadb_st_execute_iv returning imp_sth->row_num %s\n",
-                  actual_row_num);
+                  " <- mariadb_st_execute_iv returning imp_sth->row_num %" SVf "\n",
+                  SVfARG(sv_2mortal(my_ulonglong2sv(imp_sth->row_num))));
   }
 
-  return (IV)imp_sth->row_num;
+  if (imp_sth->row_num == (my_ulonglong)-1)
+    return -2; /* -2 is error */
+  else if (imp_sth->row_num <= IV_MAX)
+    return imp_sth->row_num;
+  else         /* overflow */
+    return -1; /* -1 is unknown number of rows */
 }
 
  /**************************************************************************
@@ -4776,9 +4773,8 @@ mariadb_st_fetch(SV *sth, imp_sth_t* imp_sth)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh), "\t-> mariadb_st_fetch\n");
 
   if(imp_dbh->async_query_in_flight) {
-      if(mariadb_db_async_result(sth, &imp_sth->result) <= 0) {
+      if (mariadb_db_async_result(sth, &imp_sth->result) == (my_ulonglong)-1)
         return Nullav;
-      }
   }
 
 #if MYSQL_VERSION_ID >=SERVER_PREPARE_VERSION
@@ -6395,14 +6391,14 @@ SV *mariadb_db_last_insert_id(SV *dbh, imp_dbh_t *imp_dbh,
   return sv_2mortal(my_ulonglong2sv(mysql_insert_id(imp_dbh->pmysql)));
 }
 
-int mariadb_db_async_result(SV* h, MYSQL_RES** resp)
+my_ulonglong mariadb_db_async_result(SV* h, MYSQL_RES** resp)
 {
   dTHX;
   D_imp_xxh(h);
   imp_dbh_t* dbh;
   MYSQL* svsock = NULL;
   MYSQL_RES* _res;
-  int retval = 0;
+  my_ulonglong retval = 0;
   int htype;
   bool async_sth = FALSE;
 
@@ -6436,8 +6432,8 @@ int mariadb_db_async_result(SV* h, MYSQL_RES** resp)
   dbh->async_query_in_flight = NULL;
 
   svsock= dbh->pmysql;
-  retval= mysql_read_query_result(svsock);
-  if(! retval) {
+  if (!mysql_read_query_result(svsock))
+  {
     *resp= mysql_store_result(svsock);
 
     if (mysql_errno(svsock))
@@ -6458,7 +6454,7 @@ int mariadb_db_async_result(SV* h, MYSQL_RES** resp)
       D_imp_sth(h);
       D_imp_dbh_from_sth;
 
-      if((my_ulonglong)retval+1 != (my_ulonglong)-1) {
+      if (retval != (my_ulonglong)-1) {
         if(! *resp) {
           imp_sth->insertid= mysql_insert_id(svsock);
 #if MYSQL_VERSION_ID >= MULTIPLE_RESULT_SET_VERSION
@@ -6476,7 +6472,7 @@ int mariadb_db_async_result(SV* h, MYSQL_RES** resp)
   } else {
      mariadb_dr_do_error(h, mysql_errno(svsock), mysql_error(svsock),
               mysql_sqlstate(svsock));
-     return -1;
+     return (my_ulonglong)-1;
   }
  return retval;
 }

dbdimp.h
@@ -617,7 +617,7 @@ MYSQL* mariadb_dr_connect(SV*, MYSQL*, char*, char*, char*, char*, char*,
 
 int mariadb_db_reconnect(SV*);
 
-int mariadb_db_async_result(SV* h, MYSQL_RES** resp);
+my_ulonglong mariadb_db_async_result(SV* h, MYSQL_RES** resp);
 int mariadb_db_async_ready(SV* h);
 
 int mariadb_dr_socket_ready(my_socket fd);

commit 5f08039de590ac7603e8b9f1dadb3f74b47e7492

Fix return value of $sth->rows when error occurred
Method $sth->rows should return -1 when number of rows is unknown or when
some error occurred. Before this change method $sth->rows returned
18446744073709551615 (2^64-1) or 0. Now -1.

Fixes: https://github.com/jhthorsen/mojo-mysql/pull/47#issuecomment-460202982
Fixes: aed157b4c56ee12cd40b23d2c9a3f514cca4d455

MariaDB.xs
@@ -236,6 +236,8 @@ rows(sth)
             XSRETURN_UNDEF;
         }
     }
+    if (imp_sth->row_num == (my_ulonglong)-1)
+        XSRETURN_IV(-1);
     RETVAL = my_ulonglong2sv(imp_sth->row_num);
   OUTPUT:
     RETVAL

dbdimp.c
@@ -3777,7 +3777,7 @@ mariadb_st_prepare_sv(
   imp_sth->done_desc = FALSE;
   imp_sth->result = NULL;
   imp_sth->currow = 0;
-  imp_sth->row_num = 0;
+  imp_sth->row_num = (my_ulonglong)-1;
 
   if (DBIc_TRACE_LEVEL(imp_xxh) >= 2)
     PerlIO_printf(DBIc_LOGPIO(imp_xxh),
@@ -4139,7 +4139,7 @@ bool mariadb_st_more_results(SV* sth, imp_sth_t* imp_sth)
 
   imp_sth->done_desc = FALSE;
   imp_sth->currow = 0;
-  imp_sth->row_num = 0;
+  imp_sth->row_num = (my_ulonglong)-1;
 
   /* clear NUM_OF_FIELDS attribute */
   DBIc_DBISTATE(imp_sth)->set_attr_k(sth, sv_2mortal(newSVpvs("NUM_OF_FIELDS")), 0, sv_2mortal(newSViv(0)));

t/40numrows.t
@@ -10,7 +10,7 @@ require 'lib.pl';
 my ($dbh, $sth, $aref);
 $dbh = DbiTestConnect($test_dsn, $test_user, $test_password,
                       { RaiseError => 1, PrintError => 0, AutoCommit => 0 });
-plan tests => 2*29 + 1;
+plan tests => 2*36 + 1;
 
 for my $mariadb_server_prepare (0, 1) {
 
@@ -29,6 +29,8 @@ ok $dbh->do("INSERT INTO dbd_mysql_t40numrows VALUES( 1, 'Alligator Descartes' )
 
 ok ($sth = $dbh->prepare("SELECT * FROM dbd_mysql_t40numrows WHERE id = 1"));
 
+is $sth->rows, -1, '$sth->rows prior execute is unknown (-1)';
+
 ok $sth->execute;
 
 is $sth->rows, 1, '$sth->rows should be 1';
@@ -42,6 +44,8 @@ is $sth->rows, 1, '$sth->rows still should be 1';
 ok $dbh->do("INSERT INTO dbd_mysql_t40numrows VALUES( 2, 'Jochen Wiedmann' )"), 'inserting second row';
 
 ok ($sth = $dbh->prepare("SELECT * FROM dbd_mysql_t40numrows WHERE id >= 1"));
+
+is $sth->rows, -1, '$sth->rows prior execute is unknown (-1)';
 
 ok $sth->execute;
 
@@ -57,6 +61,8 @@ ok $dbh->do("INSERT INTO dbd_mysql_t40numrows VALUES(3, 'Tim Bunce')"), "inserti
 
 ok ($sth = $dbh->prepare("SELECT * FROM dbd_mysql_t40numrows WHERE id >= 2"));
 
+is $sth->rows, -1, '$sth->rows prior execute is unknown (-1)';
+
 ok $sth->execute;
 
 is $sth->rows, 2, 'rows should be 2';
@@ -69,6 +75,8 @@ is $sth->rows, 2, 'rows still should be 2';
 
 ok ($sth = $dbh->prepare("SELECT * FROM dbd_mysql_t40numrows"));
 
+is $sth->rows, -1, '$sth->rows prior execute is unknown (-1)';
+
 ok $sth->execute;
 
 is $sth->rows, 3, 'rows should be 3';
@@ -78,6 +86,15 @@ ok ($aref= $sth->fetchall_arrayref);
 is scalar @$aref, 3, 'Verified rows should be 3';
 
 is $sth->rows, 3, 'rows still should be 3';
+
+my $sth = eval { $dbh->prepare("SYNTAX ERROR") };
+if ($sth) {
+  is $sth->rows, -1, '$sth->rows prior execute is unknown (-1)';
+  ok !eval { $sth->execute() }, '$sth->execute for SYNTAX ERROR failed';
+  is $sth->rows, -1, '$sth->rows for SYNTAX ERROR is unknown (-1)';
+} else {
+  pass '$dbh->prepare for SYNTAX ERROR failed' for 1..3;
+}
 
 ok $dbh->do("DROP TEMPORARY TABLE dbd_mysql_t40numrows"), "drop table dbd_mysql_t40numrows";
 

t/76multi_statement.t
@@ -14,7 +14,7 @@ my $dbh = DbiTestConnect($test_dsn, $test_user, $test_password,
                       { RaiseError => 1, PrintError => 0, AutoCommit => 0,
                         mariadb_multi_statements => 1 });
 
-plan tests => 71;
+plan tests => 77;
 
 ok (defined $dbh, "Connected to database with multi statement support");
 
@@ -34,6 +34,7 @@ $dbh->{mariadb_server_prepare}= 0;
   # Check that more_results works for non-SELECT results too
   my $sth;
   ok($sth = $dbh->prepare("UPDATE dbd_mysql_t76multi SET a=5 WHERE a=1; UPDATE dbd_mysql_t76multi SET a='6suffix' WHERE a<4"));
+  is($sth->rows, -1);
   ok($sth->execute(), "Execute updates");
   is($sth->rows, 1, "First update affected 1 row");
   is($sth->{mariadb_warning_count}, 0, "First update had no warnings");
@@ -98,19 +99,23 @@ $dbh->{mariadb_server_prepare}= 0;
   # Check that statement attributes are correct after calling more_results
   ok($dbh->do("CREATE TEMPORARY TABLE dbd_mysql_t76multi3 (name_id1 INT, name_id2 INT)"));
   ok($sth = $dbh->prepare("SELECT name_id1 FROM dbd_mysql_t76multi3; INSERT INTO dbd_mysql_t76multi2 VALUES(11); SELECT name_id1, name_id2 FROM dbd_mysql_t76multi3; SYNTAX ERROR"));
+  is($sth->rows, -1);
   ok($sth->execute());
   is($sth->{NUM_OF_FIELDS}, 1);
   is($sth->{NUM_OF_PARAMS}, 0);
+  is($sth->rows, 0);
   is_deeply($sth->{NAME}, [ 'name_id1' ]);
   is_deeply($sth->{mariadb_type}, [ DBD::MariaDB::TYPE_LONG ]);
   ok($sth->more_results());
   is($sth->{NUM_OF_FIELDS}, 0);
   is($sth->{NUM_OF_PARAMS}, 0);
+  is($sth->rows, 1);
   ok(!eval { $sth->{NAME} });
   ok(!eval { $sth->{mariadb_type} });
   ok($sth->more_results());
   is($sth->{NUM_OF_FIELDS}, 2);
   is($sth->{NUM_OF_PARAMS}, 0);
+  is($sth->rows, 0);
   is_deeply($sth->{NAME}, [ 'name_id1', 'name_id2' ]);
   is_deeply($sth->{mariadb_type}, [ DBD::MariaDB::TYPE_LONG, DBD::MariaDB::TYPE_LONG ]);
   ok(!eval { $sth->more_results() });
@@ -118,5 +123,6 @@ $dbh->{mariadb_server_prepare}= 0;
   is($sth->{NUM_OF_PARAMS}, 0);
   ok(!eval { $sth->{NAME} });
   ok(!eval { $sth->{mariadb_type} });
+  is($sth->rows, -1);
 
 $dbh->disconnect();